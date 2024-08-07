4e2d9465ce1c55e88dc1c8f999fbe31b15c58860
Jakub Szymsza <jakub@szymsza.cz>
2024-08-07 16:48:43 +0200

    Friends extension - display friendships

--- frontend-mu-playground/app/models/person.js
+++ frontend-mu-playground/app/models/person.js
@@ -1,6 +1,16 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class BookModel extends Model {
   @attr('string') name;
   @attr('number') age;
+
+  @hasMany('person', {
+    inverse: 'friendof',
+    async: true,
+  }) friends;
+
+  @hasMany('person', {
+    inverse: 'friends',
+    async: true,
+  }) friendof;
 }
