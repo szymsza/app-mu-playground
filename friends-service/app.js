f231b62d6a99dd3660a82a0178588db892c2a571
Jakub Szymsza <jakub@szymsza.cz>
2024-08-08 12:50:06 +0200

    Friends microservice - stealing route

--- friends-service/app.js
+++ friends-service/app.js
@@ -1,11 +1,13 @@
-import { app, query } from 'mu';
+import { app, query, sparqlEscapeUri } from 'mu';
 
+// Get some simple statistics
 app.get('/statistics', async function (req, res) {
   const getMostFriends = async (inverse = false) => {
     const queryString = `
       prefix foaf: <http://xmlns.com/foaf/0.1/>
     
-      select distinct ?person (COUNT(?friend) as ?friendCount) where {
+      SELECT DISTINCT ?person (COUNT(?friend) as ?friendCount) 
+      WHERE {
         ${
           inverse ? `
             ?friend a foaf:Person;
@@ -21,12 +23,36 @@ app.get('/statistics', async function (req, res) {
       LIMIT 1
     `;
 
-    let x = await query(queryString);
-    return x.results.bindings[0] ?? null;
+    return (await query(queryString)).results.bindings[0] ?? null;
   };
 
   res.send({
     friends: await getMostFriends(),
     friendof: await getMostFriends(true),
   });
+});
+
+// Move all friends of one person to another one
+app.get('/steal/:from/:to', async function(req, res) {
+  const { from, to } = req.params;
+
+  const queryString = `
+    prefix foaf: <http://xmlns.com/foaf/0.1/>
+  
+    DELETE {
+      ?personFrom foaf:knows ?friend
+    }
+    INSERT {
+      ?personTo foaf:knows ?friend
+    }
+    USING <http://mu.semte.ch/application>
+    WHERE {
+      VALUES ?personFrom {${sparqlEscapeUri(from)}}
+      VALUES ?personTo {${sparqlEscapeUri(to)}}
+    
+      ?personFrom foaf:knows ?friend
+    }
+  `;
+
+  res.send((await query(queryString)).results.bindings[0] ?? null);
 });
