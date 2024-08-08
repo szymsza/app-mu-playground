7ff80ae87b4e8d5b3248e09f7136024a4d7515d5
Jakub Szymsza <jakub@szymsza.cz>
2024-08-08 12:22:52 +0200

    Friends microservice - simple statistics route

--- friends-service/app.js
+++ friends-service/app.js
@@ -1,5 +1,32 @@
-import { app } from 'mu';
+import { app, query } from 'mu';
 
-app.get('/hello', function( req, res ) {
-  res.send('Hello mu-javascript-template');
-} );
+app.get('/statistics', async function (req, res) {
+  const getMostFriends = async (inverse = false) => {
+    const queryString = `
+      prefix foaf: <http://xmlns.com/foaf/0.1/>
+    
+      select distinct ?person (COUNT(?friend) as ?friendCount) where {
+        ${
+          inverse ? `
+            ?friend a foaf:Person;
+              foaf:knows ?person.
+          ` : `
+            ?person a foaf:Person;
+              foaf:knows ?friend.
+          `
+        }
+      }
+      GROUP BY ?person
+      ORDER BY DESC(?friendCount)
+      LIMIT 1
+    `;
+
+    let x = await query(queryString);
+    return x.results.bindings[0];
+  };
+
+  res.send({
+    friends: await getMostFriends(),
+    friendof: await getMostFriends(true),
+  });
+});
