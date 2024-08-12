1cc537b21ed75a826297faf504534b51ba826f4a
Jakub Szymsza <jakub@szymsza.cz>
2024-08-12 10:35:15 +0200

    Auth - link to my profile

--- frontend-mu-playground/app/controllers/auth.js
+++ frontend-mu-playground/app/controllers/auth.js
@@ -1,6 +1,30 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class AuthController extends Controller {
   @service session;
+  @service store;
+
+  @tracked
+  person = null;
+
+  constructor() {
+    super(...arguments);
+
+    this.loadUser();
+  }
+
+  async loadUser() {
+    if (!this.session.isAuthenticated) {
+      return null;
+    }
+
+    const account = await this.store.findRecord(
+      'account',
+      this.session.data.authenticated.data.relationships.account.data.id,
+    );
+
+    this.person = await account.owner;
+  }
 }
